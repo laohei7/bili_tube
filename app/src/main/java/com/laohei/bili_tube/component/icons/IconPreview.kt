8da2a450f51655e4844ca98a0c9dbfaf9f6e551b
laohei7 <laohei7@outlook.com>
2025-05-15 09:09:58 +0800

    add: 增加等级图标

--- app/src/main/java/com/laohei/bili_tube/component/icons/IconPreview.kt
+++ app/src/main/java/com/laohei/bili_tube/component/icons/IconPreview.kt
@@ -6,21 +6,64 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.util.fastForEach
+import com.laohei.bili_tube.R
 
 @OptIn(ExperimentalLayoutApi::class)
 @Preview(showBackground = true)
 @Composable
 private fun IconPreview() {
     val icons = remember {
-        listOf(Icons.Outlined.SleepTimer, Icons.Outlined.AutoSkip)
+        listOf(
+            Icons.Outlined.SleepTimer, Icons.Outlined.AutoSkip,
+        )
+    }
+    val levelIcons = remember {
+        listOf(
+            Pair(
+                Icons.Outlined.Level0,
+                R.color.level0
+            ),
+            Pair(
+                Icons.Outlined.Level1,
+                R.color.level1
+            ),
+            Pair(
+                Icons.Outlined.Level2,
+                R.color.level2
+            ),
+            Pair(
+                Icons.Outlined.Level3,
+                R.color.level3
+            ),
+            Pair(
+                Icons.Outlined.Level4,
+                R.color.level4
+            ),
+            Pair(
+                Icons.Outlined.Level5,
+                R.color.level5
+            ),
+            Pair(
+                Icons.Outlined.Level6,
+                R.color.level6
+            )
+        )
     }
     FlowRow {
         icons.fastForEach {
             Icon(
                 imageVector = it,
                 contentDescription = it.name
+            )
+        }
+        levelIcons.fastForEach {
+            Icon(
+                imageVector = it.first,
+                contentDescription = it.first.name,
+                tint = colorResource(it.second)
             )
         }
     }
---
+++ app/src/main/java/com/laohei/bili_tube/component/icons/Level4.kt
@@ -0,0 +1,102 @@
+package com.laohei.bili_tube.component.icons
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathFillType.Companion.NonZero
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.StrokeCap.Companion.Butt
+import androidx.compose.ui.graphics.StrokeJoin.Companion.Miter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.ImageVector.Builder
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.unit.dp
+
+internal val Icons.Outlined.Level4: ImageVector
+    get() {
+        if (level4 != null) {
+            return level4!!
+        }
+        level4 = Builder(
+            name = "Level4", defaultWidth = 24.0.dp, defaultHeight = 24.0.dp,
+            viewportWidth = 1024.0f, viewportHeight = 1024.0f
+        ).apply {
+            path(
+                fill = SolidColor(Color(0xFFBFBFBF)), stroke = null, strokeLineWidth = 0.0f,
+                strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
+                pathFillType = NonZero
+            ) {
+                moveTo(934.4f, 291.2f)
+                horizontalLineTo(643.2f)
+                curveToRelative(-16f, 0f, -25.6f, 12.8f, -25.6f, 25.6f)
+                verticalLineToRelative(19.2f)
+                horizontalLineTo(89.6f)
+                curveToRelative(-16f, 0f, -25.6f, 12.8f, -25.6f, 25.6f)
+                verticalLineTo(704f)
+                curveToRelative(0f, 16f, 12.8f, 25.6f, 25.6f, 25.6f)
+                horizontalLineToRelative(841.6f)
+                curveToRelative(16f, 0f, 25.6f, -12.8f, 25.6f, -25.6f)
+                verticalLineTo(316.8f)
+                curveToRelative(3.2f, -12.8f, -9.6f, -25.6f, -22.4f, -25.6f)
+                close()
+
+                moveTo(307.2f, 662.4f)
+                curveToRelative(0f, 9.6f, -9.6f, 19.2f, -19.2f, 19.2f)
+                horizontalLineTo(137.6f)
+                curveToRelative(-9.6f, 0f, -19.2f, -9.6f, -19.2f, -19.2f)
+                verticalLineTo(409.6f)
+                curveToRelative(0f, -9.6f, 9.6f, -19.2f, 19.2f, -19.2f)
+                horizontalLineToRelative(25.6f)
+                curveToRelative(9.6f, 0f, 19.2f, 9.6f, 19.2f, 19.2f)
+                verticalLineToRelative(208f)
+                horizontalLineToRelative(108.8f)
+                curveToRelative(9.6f, 0f, 19.2f, 9.6f, 19.2f, 19.2f)
+                verticalLineToRelative(25.6f)
+                close()
+
+                moveTo(579.2f, 563.2f)
+                verticalLineToRelative(6.4f)
+                curveToRelative(0f, 6.4f, 0f, 12.8f, -3.2f, 16f)
+                lineToRelative(-89.6f, 89.6f)
+                curveToRelative(-6.4f, 6.4f, -22.4f, 6.4f, -22.4f, 6.4f)
+                reflectiveCurveToRelative(-16f, 0f, -22.4f, -6.4f)
+                lineTo(352f, 585.6f)
+                curveToRelative(-6.4f, -3.2f, -6.4f, -12.8f, -3.2f, -19.2f)
+                verticalLineTo(409.6f)
+                curveToRelative(0f, -9.6f, 9.6f, -19.2f, 19.2f, -19.2f)
+                horizontalLineToRelative(25.6f)
+                curveToRelative(9.6f, 0f, 19.2f, 9.6f, 19.2f, 19.2f)
+                verticalLineToRelative(147.2f)
+                lineToRelative(54.4f, 54.4f)
+                lineToRelative(54.4f, -54.4f)
+                verticalLineToRelative(-144f)
+                curveToRelative(0f, -9.6f, 9.6f, -19.2f, 19.2f, -19.2f)
+                horizontalLineToRelative(25.6f)
+                curveToRelative(9.6f, 0f, 19.2f, 9.6f, 19.2f, 19.2f)
+                verticalLineToRelative(150.4f)
+                close()
+
+                moveTo(905.6f, 662.4f)
+                curveToRelative(0f, 9.6f, -9.6f, 19.2f, -19.2f, 19.2f)
+                horizontalLineToRelative(-25.6f)
+                curveToRelative(-9.6f, 0f, -19.2f, -9.6f, -19.2f, -19.2f)
+                verticalLineTo(544f)
+                horizontalLineTo(688f)
+                curveToRelative(-9.6f, 0f, -19.2f, -9.6f, -19.2f, -19.2f)
+                verticalLineTo(361.6f)
+                curveToRelative(0f, -9.6f, 9.6f, -19.2f, 19.2f, -19.2f)
+                horizontalLineToRelative(25.6f)
+                curveToRelative(9.6f, 0f, 19.2f, 9.6f, 19.2f, 19.2f)
+                verticalLineTo(480f)
+                horizontalLineToRelative(108.8f)
+                verticalLineTo(361.6f)
+                curveToRelative(0f, -9.6f, 9.6f, -19.2f, 19.2f, -19.2f)
+                horizontalLineToRelative(25.6f)
+                curveToRelative(9.6f, 0f, 19.2f, 9.6f, 19.2f, 19.2f)
+                verticalLineTo(662.4f)
+                close()
+            }
+        }.build()
+        return level4!!
+    }
+
+private var level4: ImageVector? = null
