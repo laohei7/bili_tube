c1d3e4c14b77ec9be838db84974426485b32d018
laohei7 <laohei7@outlook.com>
2025-06-03 12:08:41 +0800

    fix: 修复杜比视频profile5画质播放失败问题

--- app/src/main/java/com/laohei/bili_tube/presentation/player/state/media/MediaManager.kt
+++ app/src/main/java/com/laohei/bili_tube/presentation/player/state/media/MediaManager.kt
@@ -2,16 +2,12 @@ package com.laohei.bili_tube.presentation.player.state.media
 
 import androidx.media3.datasource.DataSource
 import androidx.media3.exoplayer.ExoPlayer
-import androidx.media3.exoplayer.source.MediaSource
 import com.laohei.bili_sdk.module_v2.video.VideoURLModel
-import com.laohei.bili_tube.model.VideoSource
 import kotlinx.coroutines.flow.StateFlow
 
 internal interface MediaManager {
     val state: StateFlow<MediaState>
 
-    fun play(mediaSource: MediaSource)
-    fun play(videoSources: List<VideoSource>, dataSourceFactory: DataSource.Factory? = null)
     fun play(videoURLModel: VideoURLModel, dataSourceFactory: DataSource.Factory? = null)
     fun play(video: String, audio: String?)
     fun seekTo(duration: Long)
