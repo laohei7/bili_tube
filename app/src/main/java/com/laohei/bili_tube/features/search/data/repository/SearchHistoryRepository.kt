de387b1a0784ba9c08869a8a361e03715ad52ec0
laohei7 <laohei7@outlook.com>
2025-08-21 09:19:57 +0800

    add: add local search history

---
+++ app/src/main/java/com/laohei/bili_tube/features/search/data/repository/SearchHistoryRepository.kt
@@ -0,0 +1,28 @@
+package com.laohei.bili_tube.features.search.data.repository
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.laohei.bili_tube.room.dao.SearchHistoryDao
+import com.laohei.bili_tube.room.entity.SearchHistory
+import kotlinx.coroutines.flow.Flow
+
+class SearchHistoryRepository(
+    private val searchHistoryDao: SearchHistoryDao
+) {
+    fun searchHistoryPager(): Flow<PagingData<SearchHistory>> {
+        return Pager(
+            config = PagingConfig(
+                pageSize = 20,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = { searchHistoryDao.getHistoryPaging() }
+        ).flow
+    }
+
+    suspend fun addSearchHistoryItem(item: SearchHistory) {
+        val exist = searchHistoryDao.findByKeyword(item.keyword)
+        exist?.let { searchHistoryDao.update(it.copy(timestamp = item.timestamp)) }
+            ?: run { searchHistoryDao.insert(item) }
+    }
+}
