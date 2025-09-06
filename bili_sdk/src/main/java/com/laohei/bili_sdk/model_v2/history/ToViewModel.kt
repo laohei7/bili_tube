58190d62fbdd0c72821223bbb2d5a89e558c248d
laohei7 <laohei7@outlook.com>
2025-09-06 11:15:16 +0800

    update: refactor package name in bili_sdk module

---
+++ bili_sdk/src/main/java/com/laohei/bili_sdk/model_v2/history/ToViewModel.kt
@@ -0,0 +1,18 @@
+package com.laohei.bili_sdk.model_v2.history
+
+import com.laohei.bili_sdk.model_v2.video.VideoView
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonIgnoreUnknownKeys
+
+@OptIn(ExperimentalSerializationApi::class)
+@Serializable
+@JsonIgnoreUnknownKeys
+data class ToViewModel(
+    val count: Int,
+    val list: List<VideoView>
+) {
+    companion object {
+        val EMPTY = ToViewModel(0, emptyList())
+    }
+}
